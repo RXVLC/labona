2ea9e37ae0683f11777be32f99a93c1610dfbaa5
rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-25 21:20:48 +0100

    Hemos realizado lo que sería subjects, a falta de traducirlo a Valenciano,

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AW
 La url de esta pagina es: https://rafetaavlc.github.io/AW/
+
+Hemos sacado los iconos de: https://iconos8.es/
--- tablas_subject.js
+++ tablas_subject.js
@@ -17,18 +17,18 @@ function TSMR1() {
 }
 
 function TSMR2() {
-  var x = document.getElementById("tsmr2");
-  if (x.style.display === "block") {
-      x.style.display = "none";
-  } else {
-      x.style.display = "block";
-  }
+    var x = document.getElementById("tsmr2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 
-  var y = document.getElementById("fondo_azul_por_footer");
-  if (y.style.display === "block") {
-      y.style.display = "none";
-  } else {
-      y.style.display = "block";
-  }
+    var y = document.getElementById("fondo_azul_por_footer");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
 
 }
