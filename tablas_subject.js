7945b4360cc7912f7782e12b3fb19b241def9ae4
rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-25 16:48:18 +0100

    script para los subjects acabado y seguimos para bingo!

--- tablas_subject.js
+++ tablas_subject.js
@@ -1,11 +1,18 @@
 
 
 function TSMR1() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("grid1");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
+    }
+
+    var y = document.getElementById("fondo_azul_por_footer");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
     }
 }
 
@@ -16,4 +23,12 @@ function TSMR2() {
   } else {
       x.style.display = "block";
   }
+
+  var y = document.getElementById("fondo_azul_por_footer");
+  if (y.style.display === "block") {
+      y.style.display = "none";
+  } else {
+      y.style.display = "block";
+  }
+
 }
