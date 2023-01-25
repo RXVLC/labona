d37732abcf8b109692f334ada2efb0888b105f73
Rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-25 12:51:07 +0100

    pendiente de acabar los grids de subjects

--- tablas_subject.js
+++ tablas_subject.js
@@ -1,56 +1,19 @@
-  // Mostrar y ocultar listado de planes de financiación
-  var element = document.getElementById("oldNews");
-  if(element != null){
-      element.style.display = "none";
-  }
-  var element = document.getElementById("old2");
-  if(element != null){
-      element.style.display = "none";
-  }
-  
-  function showHide(elementId) {
-      var element = document.getElementById(elementId);
-      if (element != null) {
-          if(element.style.display != "none"){
-              element.style.display = "none";
-          }else{
-              element.style.display = "block";
-          }
-      }
-  }
 
 
-
-//   para el botón de las asignaturas
-function myFunction(paco) {
-  document.getElementById("myDropdown_subjects").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn_subjects')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content_subjects");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+function TSMR1() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
-  }
 }
 
-
-const btns = document.querySelectorAll('.btn');
-
-if(btns){
-	btns.forEach(item => {
-  	item.addEventListener('click', function(event){
-    	let id = event.target.getAttribute('data-id');
-      if(id){
-      	document.querySelector('.active').classList.remove('active');
-      	document.getElementById(id).classList.add('active');
-      }
-    })
-  })
+function TSMR2() {
+  var x = document.getElementById("tsmr2");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
 }
