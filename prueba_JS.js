e80dd7a6a561117d6df87f06ce1348db39ba96de
rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-26 16:55:13 +0100

    SUBJECTS BIEN , NECESITAMOS AYUDA EN SUPERIOR

--- prueba_JS.js
+++ prueba_JS.js
@@ -1,69 +1,86 @@
 
-    var fondo = document.getElementById("fondo_azul_por_footer");
-    var x = document.getElementById("grid1");
-    var y = document.getElementById("tsmr2");
-    var j = 7;
+
+
+// function TSMR1() {
+//     var grid1 = document.getElementById("grid1");
+//     var foot = document.getElementById("foot");
+//     var grid2 = document.getElementById("tsmr2");
+// // cuando se pulse el botón:
+
+// // el primer if funciona, hace que si estas en el 2 y pulsas para ver el uno se 
+// // quite automáticamente el 2
+
+
+// if (grid1.style.display = "none"){
+//     grid1.style.display = "block";
+//     foot.style.marginTop = "0px";
+//     alert("asdsa")
+// }
+// else{
+//     foot.style.marginTop = "800px";
+//     grid1.style.display = "none";
+//     alert("pak0")
+// }
+
     
-    var 1 = 0;
-    var 2 = 0;
 
-    if(y.style.display === "none"){
-        let 1 = 0;
-    }
-    else{
-        let 1 = 1;
-    }
 
-    if(y.style.display === "none"){
-        let 2 = 0
-    }
-    else{
-        let 2 = 1
-    }
+    
+
+//     }
 
 
     function TSMR1() {
-        if (y.style.display === "block" && fondo.style.display === "block") {
-            y.style.display === "none"
-            fondo.style.display === "none"
-            x.style.display === "block"
+        var grid1 = document.getElementById("grid1");
+        var foot = document.getElementById("foot");
+        var grid2 = document.getElementById("tsmr2");
+    // cuando se pulse el botón:
+    
+    // el primer if funciona, hace que si estas en el 2 y pulsas para ver el uno se 
+    // quite automáticamente el 2
+    
+    
+        if (grid2.style.display = "block") {
+            grid2.style.display = "none";
+            foot.style.marginTop = "0px";
+            grid1.style.display = "block";
+            
         }
-        else{
-            y.style.display === "block"
-            fondo.style.display ==="block"
-        }
-        while (j = 7) {
-            alert("paco")
-            if (x.style.display === "block") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "block";
+        else if (grid1.style.display = "block"){
+                grid1.style.display = "none";
+                foot.style.marginTop = "800px";
             }
-        }
         
-
-        var y = document.getElementById("fondo_azul_por_footer");
-        if (y.style.display === "block") {
-            y.style.display = "none";
-        } else {
-            y.style.display = "block";
+        else {
+            foot.style.marginTop = "0px";
+            grid1.style.display = "block";
         }
-    }
+        }
+
+
+    
+
+
+
+
 
     function TSMR2() {
-        var x = document.getElementById("tsmr2");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        } 
+        var grid1 = document.getElementById("grid1");
+        var foot = document.getElementById("foot");
+        var grid2 = document.getElementById("tsmr2");
+        if (grid1.style.display = "block") {
+            grid1.style.display = "none";
+            foot.style.marginTop = "0px";
+            grid2.style.display = "block";
             
-
-        var y = document.getElementById("fondo_azul_por_footer");
-        if (y.style.display === "block") {
-            y.style.display = "none";
-        } else {
-            y.style.display = "block";
         }
-
+        else if (grid2.style.display = "block"){
+                grid2.style.display = "none";
+                foot.style.marginTop = "800px";
+            }
+        
+        else {
+            foot.style.marginTop = "0px";
+            grid2.style.display = "block";
+        }
 }
