1d4ea009a343247f7c7c9467b3a2428a53bdade6
Rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-26 12:47:57 +0100

    todo bien hasta ahora, falta acabar los botones y traducir

--- tablas_subject.js
+++ tablas_subject.js
@@ -1,34 +1,69 @@
 
-
-function TSMR1() {
+    var fondo = document.getElementById("fondo_azul_por_footer");
     var x = document.getElementById("grid1");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+    function TSMR1() {
+     var x = document.getElementById("grid1");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+
+        var y = document.getElementById("fondo_azul_por_footer");
+        if (y.style.display === "block") {
+            y.style.display = "none";
+        } else {
+            y.style.display = "block";
+        }
+    }
+    function TSMR2() {
+        var x = document.getElementById("tsmr2");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+        var y = document.getElementById("fondo_azul_por_footer");
+        if (y.style.display === "block") {
+            y.style.display = "none";
+        } else {
+            y.style.display = "block";
+        }
+
     }
 
-    var y = document.getElementById("fondo_azul_por_footer");
-    if (y.style.display === "block") {
-        y.style.display = "none";
-    } else {
-        y.style.display = "block";
-    }
-}
 
-function TSMR2() {
-    var x = document.getElementById("tsmr2");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+    var fondo = document.getElementById("fondo_azul_por_footer");
+    var x = document.getElementById("grid1");
+    function TSMR1() {
+        var x = document.getElementById("grid1");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+
+        var y = document.getElementById("fondo_azul_por_footer");
+        if (y.style.display === "block") {
+            y.style.display = "none";
+        } else {
+            y.style.display = "block";
+        }
     }
 
-    var y = document.getElementById("fondo_azul_por_footer");
-    if (y.style.display === "block") {
-        y.style.display = "none";
-    } else {
-        y.style.display = "block";
-    }
+    function TSMR2() {
+        var x = document.getElementById("tsmr2");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";}
+            
+
+        var y = document.getElementById("fondo_azul_por_footer");
+        if (y.style.display === "block") {
+            y.style.display = "none";
+        } else {
+            y.style.display = "block";
+        }
 
 }
