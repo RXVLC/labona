d95b94745833b3f5e70ad2cad5fdb98308ac0f77
rafetaavlc <ranagi@floridauniversitaria.es>
2023-01-29 18:23:49 +0100

    algo hay cambiado

--- tablas_subject.js
+++ tablas_subject.js
@@ -2,8 +2,8 @@
     var fondo = document.getElementById("fondo_azul_por_footer");
     var x = document.getElementById("grid1");
     var footer = document.getElementById("foot")
-    function TSMR1() {
-     var x = document.getElementById("grid1");
+    function DAW() {
+     var x = document.getElementById("tsmr2");
         if (x.style.display === "block") {
             x.style.display = "none";
         } else {
@@ -33,39 +33,3 @@
 
     }
 
-
-    var fondo = document.getElementById("fondo_azul_por_footer");
-    var x = document.getElementById("grid1");
-    function TSMR1() {
-        var x = document.getElementById("grid1");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-
-        var y = document.getElementById("fondo_azul_por_footer");
-        if (y.style.display === "block") {
-            y.style.display = "none";
-        } else {
-            y.style.display = "block";
-        }
-    }
-
-    function TSMR2() {
-        var x = document.getElementById("tsmr2");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        } 
-            
-
-        var y = document.getElementById("fondo_azul_por_footer");
-        if (y.style.display === "block") {
-            y.style.display = "none";
-        } else {
-            y.style.display = "block";
-        }
-
-}
